00fd0784ddbf21b5389d60626738c8885b6b10aa
Júnior Nunes <denisonprobable13@gmail.com>
2022-03-19 15:13:18 -0300

    crated hello-world in home screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {
+    Container,
+    Title
+} from './styles'
+
+interface Props{
+    title: string;
+}
+
+export function Home(){
+
+    return (
+        <Container>
+            <Title> Hello World! </Title>
+        </Container>
+    );
+}
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,6 +1,12 @@
 import styled from 'styled-components/native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+    font-size: ${RFValue(50)}px;
+`;
