62833fb5b811b982a1665653b472d74852f55855
Júnior Nunes <denisonprobable13@gmail.com>
2022-03-21 09:55:20 -0300

    html formatted

--- src/screens/Post/index.tsx
+++ src/screens/Post/index.tsx
@@ -6,6 +6,7 @@ import React, { useEffect, useState } from 'react';
 import RenderHTML from 'react-native-render-html';
 import AppLoading from 'expo-app-loading';
 import { useRoute } from '@react-navigation/native';
+import { useTheme } from 'styled-components';
 import {
   Container,
   Content,
@@ -32,6 +33,8 @@ export function Post() {
   const { getPost } = usePosts();
   const route = useRoute();
   const { postId } = route.params as Params;
+  const theme = useTheme();
+  const { text } = theme.colors;
 
   useEffect(() => {
     async function onGetPost() {
@@ -45,7 +48,32 @@ export function Post() {
     const getPostInformations = () => {
       if (!post || post.id === undefined) return;
       const source = {
-        html: post.content.rendered,
+        html: `
+					<!DOCTYPE html>
+					<html>
+					<head>
+							<meta charset="UTF-8">
+							<meta http-equiv="X-UA-Compatible" content="IE=edge">
+							<meta name="viewport" content="width=device-width, initial-scale=1.0">
+							<style>
+									body {
+										color: ${text}
+									}
+							</style>
+							<title>Document</title>
+					</head>
+					<body>
+							${
+  post.content.rendered
+    .replace(/<b/g, `<b style="color:${text}"`)
+    .replace(/<l/g, `<b style="color:"${text}"`)
+    .replace(/<span/g, `<span style="color:${text}"`)
+    .replace(/<strong/g, `<strong style="color:${text}"`)
+    .replace(/<li/g, `<li style="color:${text}"`)
+}
+					</body>
+					</html>
+				`,
       };
       setPostTextHTML(source);
       setPostTitle(post.title.rendered);
